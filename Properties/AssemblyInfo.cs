d06aec063f48a4638569b81850c049ad298fd05d
touya <touyahokonohara4@gmail.com>
2026-06-27 20:24:59 +0900

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,19 +1,14 @@
-using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using System.Security;
-using System.Security.Permissions;
 
 [assembly: AssemblyTitle("NX Macro Controller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NX Macro Controller for Nintendo Switch")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NX Macro Controller")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("79bdea46-c185-4207-ab53-b28244bbc80d")]
-[assembly: AssemblyFileVersion("2.0.13.0")]
-[assembly: AssemblyVersion("2.0.13.0")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
